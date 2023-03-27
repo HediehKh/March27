5b8c69f07c8e06586da968e3cad07023f1751933
HediehKh <113653779+HediehKh@users.noreply.github.com>
2023-03-27 11:25:14 -0700

    submit

---
+++ data/matrix.js
@@ -0,0 +1,32 @@
+export const matrix = {
+    courses: [
+        {
+            term: 1,
+            code: "COMM 1118",
+            title: "Communication for digital design and development 1",
+            credits: 3.0,
+            description: "This is a description... "
+        },
+        {
+            term: 2,
+            code: "COMM 2118",
+            title: "Communication for digital design and development 1",
+            credits: 4.0,
+            description: "This is a description... "
+        },
+        {
+            term: 3,
+            code: "COMM 3118",
+            title: "Front end web development",
+            credits: 3.0,
+            description: "This is a description... "
+        },
+        {
+            term: 4,
+            code: "BLAW 3205",
+            title: "Internet and IT law",
+            credits: 3.0,
+            description: "This is a description... "
+        },
+    ]
+}
